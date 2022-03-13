34466da777283da25f190c7b6b5f895c18341dc7
kapilmehtani <44068436+kapilmehtani@users.noreply.github.com>
2022-03-13 21:41:32 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { BusAppComponent } from './bus-app/bus-app.component';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
